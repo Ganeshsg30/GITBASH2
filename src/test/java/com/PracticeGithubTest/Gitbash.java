b3b09f4a13c731ffde1ee0b506cbabd0191e47f3
Ganeshsg30 <ganeshsg1996@gmail.com>
2022-06-02 15:03:08 +0530

    push by venky

--- src/test/java/com/PracticeGithubTest/Gitbash.java
+++ src/test/java/com/PracticeGithubTest/Gitbash.java
@@ -4,13 +4,7 @@ public class Gitbash
 {
 	public static void m1()
 	{
-		System.out.println("Gitbash2");
-
-		System.out.println("fuck person 1");
-
-
-//pppppppppppppppppppppppppppp
-
+		System.out.println("Gitbash2 by venky");
 	}
 }
 
